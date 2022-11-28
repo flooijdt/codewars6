f1149a2603bb30e5d235f611ec9e94588af92c10
flooijdt <flooijdt@localhost.localdomain>
2022-11-28 08:25:22 -0300

    made it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+fn likes(names: &[&str]) -> String {
+    if names.is_empty() {
+        format!("no one likes this")
+    } else {
+        if names.len() == 1 {
+            format!("{}, likes this", names[0])
+        } else {
+            if names.len() == 2 {
+                format!("{} and {} like this", names[0], names[1])
+            } else if names.len() == 3 {
+                format!("{}, {} and {} like this", names[0], names[1], names[2])
+            } else {
+                format!(
+                    "{}, {}, and {} others like this",
+                    names[0],
+                    names[1],
+                    names.len() - 2
+                )
+            }
+        }
+    }
 }
